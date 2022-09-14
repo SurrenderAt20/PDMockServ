b1407a2d9ecbf3bfa5b7d5feb712c11a5b525655
SurrenderAt20 <54646142+SurrenderAt20@users.noreply.github.com>
2022-09-14 11:17:47 +0200

    data rendering useEffect

--- src/components/Users/User.tsx
+++ src/components/Users/User.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import "./User.css"
+import { ConvertedEmployee } from 'ts/interfaces';
 
-export default function User(props: any) {
+export default function User(props: ConvertedEmployee) {
 
     return (
-          <div className="list__item">
+          <div key={props.id} className="list__item">
             <h4>{props.fullName}</h4>
             <h4>{props.birthday}</h4>
             <h4>{props.salary}</h4>
