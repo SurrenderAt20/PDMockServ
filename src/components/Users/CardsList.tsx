f2cc5778db157ed8413535c8a15ddb5ef30f5ce3
SurrenderAt20 <54646142+SurrenderAt20@users.noreply.github.com>
2022-10-02 20:58:48 +0200

    attempt from this friday on extracting initials

--- src/components/Users/CardsList.tsx
+++ src/components/Users/CardsList.tsx
@@ -3,12 +3,24 @@ import { ConvertedEmployee } from "ts/interfaces";
 import "./CardsList.css";
 
 export default function CardsList(props: any) {
+  const getInitials = (string: any) => {
+    const names = string.split(` `);
+    let initials = names[0].substring(0, 1).toUpperCase;
+
+    if (names.length > 1) {
+      initials += names[names.length - 1].substring(0, 1).toUpperCase();
+    }
+    return initials;
+  };
+
   return (
     <div className="container__container">
-    <div className="cards__container">
+      <div className="cards__container">
         {props.userData?.map((user: ConvertedEmployee) => (
           <div key={user.id} className="list__item__card">
-            <div><h3>{user.initials}</h3></div>
+            <div>
+              <h3>{getInitials}</h3>
+            </div>
             <h4>{user.fullName}</h4>
             <h4>{user.birthday}</h4>
             <h4>{user.salary}</h4>
@@ -16,6 +28,6 @@ export default function CardsList(props: any) {
           </div>
         ))}
       </div>
-      </div>
+    </div>
   );
 }
